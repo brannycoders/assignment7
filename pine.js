b01c8dfc178f1de9d19905671a7bdc80409935e7
brannycoders <nathanielbn@gmail.com>
2024-07-16 10:34:35 +0100

    new commit

---
+++ pine.js
@@ -0,0 +1,6 @@
+// const menuIcon = document.querySelector('.menu-icon');
+// const navList = document.querySelector('.nav-list');
+
+// menuIcon.addEventListener('click', () => {
+//     navList.classList.toggle('active');
+// });
